24bbda2dfd83b644e7f80237325043bfbe6ae372
IreneFox2025 <bronnikova702@gmail.com>
2026-09-25 20:35:32 +0600

    popup

--- script/script.js
+++ script/script.js
@@ -1,9 +1,14 @@
 const prev = document.querySelector(".swiper-button-prev");
 const next = document.querySelector(".swiper-button-next");
 
+const popup = document.querySelector("#popup");
+const button = document.querySelector("#order_service");
+
+const close_btn = document.querySelector(".popup__close_btn");
+const popup_content = document.querySelector("#popup_content");
+
 prev.style.backgroundImage = "url('./assets/pictures/prev.png')";
 next.style.backgroundImage = "url('./assets/pictures/next.png')";
-
 
 const galleryInfo = [
   "Проект двухкомнатной квартиры в ЖК Лесной Пейзаж",
@@ -27,3 +32,28 @@ const swiper = new Swiper(".swiper", {
     },
   },
 });
+
+button.addEventListener("click", () => {
+  popup.showModal();
+  document.body.style.overflow = 'hidden';
+});
+
+close_btn.addEventListener("click", () => {
+  popup.close();
+  document.body.style.overflow = '';
+});
+
+popup.addEventListener("click", (event) => {
+  const rect = popup_content.getBoundingClientRect();
+
+  if (
+    event.clientX < rect.left ||
+    event.clientX > rect.right ||
+    event.clientY < rect.top ||
+    event.clientY > rect.bottom
+  ) {
+    popup.close();
+    document.body.style.overflow = '';
+  }
+});
+
